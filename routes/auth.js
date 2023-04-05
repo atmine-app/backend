599c951624f87fdd385b92f12c897215da111a46
guille garcia <ggarciacardiel@gmail.com>
2023-04-05 17:05:16 +0200

    adds payment email confirmation fo bookings

--- routes/auth.js
+++ routes/auth.js
@@ -49,7 +49,7 @@ router.post('/signup', async (req, res, next) => {
       const mailOptions = {
         from: `"atmine" <${process.env.TRANSPORTER_EMAIL}>`,
         to: email,
-        subject: "Welcome to atmine!",
+        subject: "👋 Welcome to atmine!",
         html: `
         <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
         <html xmlns="http://www.w3.org/1999/xhtml"
